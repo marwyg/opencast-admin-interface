ea926e1f4849b6d15ae984fa6dfac853e868c923
Martin Wygas <wygas@elan-ev.de>
2025-11-04 14:53:01 +0100

    Get GitHub Workflows from main branch

--- .github/get-release-server.sh
+++ .github/get-release-server.sh
@@ -12,9 +12,7 @@ cd ~/opencast
 #Get the list of *all* branches in the format remotes/origin/r/N.m
 #grep for r/N.x
 #then use cut to remove remotes/origin
-#then use sort, with a field delimiter of '/', sorting on the *second* key in 'n'umeric 'r'evers order
-#then only consider the first 3 entries
-ary=( develop `git branch -a | grep origin | grep 'r/[0-9]*.x' | cut -f 3- -d '/' | sort -t '/' -k 2nr | head -n 3` )
+ary=( `git branch -a | grep 'r/[0-9]*.x' | head -n 3 | cut -f 3- -d '/'` )
 
 #Iterate through the array above.
 #If the script input matches the first item, spit out develop
@@ -22,16 +20,13 @@ ary=( develop `git branch -a | grep origin | grep 'r/[0-9]*.x' | cut -f 3- -d '/
 #If it doesn't match anything, then don't say anything
 for i in "${!ary[@]}"
 do
-  if [[ "${ary[i]}" = "$1" ]]; then
+  if [[ "${ary[$i]}" = "$1" ]]; then
     if [[ $i -eq 0 ]]; then
       echo "develop.opencast.org"
-      exit 0
     elif [[ $i -eq 1 ]]; then
       echo "stable.opencast.org"
-      exit 0
     elif [[ $i -eq 2 ]]; then
       echo "legacy.opencast.org"
-      exit 0
     fi
   fi
 done
